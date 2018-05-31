288a2e20bb5b4bed7eafc69eb74be55bd75f6fc9
Daniel <gamerpcx@outlook.com>
2018-05-31 03:44:22 -0700

    Player.cs
    Player

--- BackatSchool/Player.cs
+++ BackatSchool/Player.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
 
 namespace BackatSchool
 {
